12aec9988afa31abc1a997f9135bab95b1d52741
Yuqiwu <ywu@stuy.edu>
2018-02-26 15:26:50 -0500

    stop at method

--- hw06.py
+++ hw06.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_tempalte, url_for
+from flask import Flask, render_template, redirect, request
 import json
 import pymongo
+import notwalylime
 
 app = Flask(__name__)
 
@@ -15,20 +16,35 @@ for episode in dic["_embedded"]["episodes"]:
     
 
 connection = pymongo.MongoClient("149.89.150.100")
-db.notwalylime.drop()
 db = connection.notwalylime
+db.rick_and_morty.drop()
 
 for each in final:
-    print each
     db.rick_and_morty.insert( each )
 
 @app.route("/")
 def root():
-    return render_templates("form.html")
+    s1 = []
+    s2 = []
+    s3 = []
+    for episode in notwalylime.find_season(1):
+        s1.append(episode["number"])
+    for episode in notwalylime.find_season(2):
+        s2.append(episode["number"])
+    for episode in notwalylime.find_season(3):
+        s3.append(episode["number"])
+    return render_template("form.html", season1=s1,season2=s2,season3=s3)
 
 @app.route("/result", methods=["GET","POST"])
 def result():
-    season = request.form['value']
-    return render_templates("list.html")
+    season = int(request.form['value'])
+    episode = int(request.form['name'])
+    print season
+    print episode
+    result = notwalylime.find_season_episode(season, episode)
+    url = episode['url']
+    return redirect(url)
 
-
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
